dbae54e944412ad9572bd26f1b07563b71388819
ulna <xianxingyuan520@outlook.com>
2025-06-20 14:44:25 +0800

    Remove build directory from tracking and add .gitignore

--- build/CMakeFiles/my_DNS.dir/src/DNScache/relayBuild.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/my_DNS.dir/src/DNScache/relayBuild.c.o: \
- /home/ulna/桌面/Windows/windows/my_DNS/src/DNScache/relayBuild.c \
- /usr/include/stdc-predef.h \
- /home/ulna/桌面/Windows/windows/my_DNS/include/DNScache/relayBuild.h
